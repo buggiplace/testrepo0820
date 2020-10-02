b7e9339e15cc458d55ac8886788758c162721c03
Moritz Bugiel <mc.bugiel@googlemail.com>
2020-10-02 15:30:54 +0200

    5th question added

--- prep19.rb
+++ prep19.rb
@@ -10,12 +10,14 @@ p1 = "What color has the Le Wagon Logo?\n(a)red\n(b)purple\n(c)orange"
 p2 = "Where is Le Wagon situated in Berlin?\n(a)Kreuzberg\n(b)Mitte\n(c)Spandau"
 p3 = "What is the best language?\n(a)Python\n(b)C#\n(c)Ruby"
 p4 = "getting jiggy with it?\n(a)Nananana\n(b)Lalalalala\n(c)Holla"
+p5 = "Who let the dogs out\n(a)Trump\n(b)Ibrahimovic\n(c)Their owner"
 
 questions = [
   Question.new(p1,"a"),
   Question.new(p2,"b"),
   Question.new(p3,"c"),
-  Question.new(p4,"a")
+  Question.new(p4,"a"),
+  Question.new(p5,"c")
 ]
 
 
