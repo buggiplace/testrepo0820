9c9fe68ea3e044476893de42d127675408eec1d2
Moritz Bugiel <mc.bugiel@googlemail.com>
2020-10-02 15:47:06 +0200

    Question 6 added

--- prep19.rb
+++ prep19.rb
@@ -11,13 +11,15 @@ p2 = "Where is Le Wagon situated in Berlin?\n(a)Kreuzberg\n(b)Mitte\n(c)Spandau"
 p3 = "What is the best language?\n(a)Python\n(b)C#\n(c)Ruby"
 p4 = "getting jiggy with it?\n(a)Nananana\n(b)Lalalalala\n(c)Holla"
 p5 = "Who let the dogs out\n(a)Trump\n(b)Ibrahimovic\n(c)Their owner"
+p6 = "I am singing\n(a)Loud\n(b)in the rain\n(c)never"
 
 questions = [
   Question.new(p1,"a"),
   Question.new(p2,"b"),
   Question.new(p3,"c"),
   Question.new(p4,"a"),
-  Question.new(p5,"c")
+  Question.new(p5,"c"),
+  Question.new(p6,"b")
 ]
 
 
