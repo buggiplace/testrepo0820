a788a9e44d72db9eda1b623111599db4f119439c
Moritz Bugiel <mc.bugiel@googlemail.com>
2020-10-02 15:58:25 +0200

    changed answer in p5

--- prep19.rb
+++ prep19.rb
@@ -10,7 +10,7 @@ p1 = "What color has the Le Wagon Logo?\n(a)red\n(b)purple\n(c)orange"
 p2 = "Where is Le Wagon situated in Berlin?\n(a)Kreuzberg\n(b)Mitte\n(c)Spandau"
 p3 = "What is the best language?\n(a)Python\n(b)C#\n(c)Ruby"
 p4 = "getting jiggy with it?\n(a)Nananana\n(b)Lalalalala\n(c)Holla"
-p5 = "Who let the dogs out\n(a)Trump\n(b)Ibrahimovic\n(c)Their owner"
+p5 = "Who let the dogs out\n(a)Trump\n(b)A cat\n(c)Their owner"
 p6 = "I am singing\n(a)Loud\n(b)in the rain\n(c)never"
 
 questions = [
